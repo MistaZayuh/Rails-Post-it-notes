d750aeb8baa4bf139dcc82da994d8df8d3196d46
Isaiah Boren <MistaZayuh@gmail.com>
2019-08-19 17:26:56 -0600

    added CRUD

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,16 +21,27 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
 
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
 
-
-
-
-
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
 
   private
 
-  def pages_params
+  def notes_params
     params.require(:note).permit(:title, :author, :body)
   end
 
